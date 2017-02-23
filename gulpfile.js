21415e809cd109d499cbab716192861d00c87ebe
Ratan Sunder Parai <ratanparai@gmail.com>
2017-02-23 10:12:29 +0600

    using pug to generate html

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,55 @@
 var gulp = require('gulp');
+var pug = require('gulp-pug');
+
+// file location
+var options = {
+	angular : {
+		src : './node_modules/angular/angular.js',
+		srcMin : './node_modules/angular/angular.min.js',
+		dest : 'public/js/'
+	},
+	pug : {
+		src : 'src/pug/**/*.pug',
+		dest : 'public/'
+	}
+};
 
 // Gulp task to copy required files to public folder so that we can use them
 // on our web app
 gulp.task('copy', function(){
-    gulp.src('./node_modules/angular/angular.min.js')
-        .pipe(gulp.dest('./public/js/'));
+    return gulp.src(options.angular.srcMin)
+        .pipe(gulp.dest(options.angular.dest));
 
 });
 
-gulp.task('default', ['copy']);
+
+/* pug
+ * ----*/
+gulp.task('pug', function(){
+	return gulp.src(options.pug.src)
+		.pipe(pug({
+			pretty : true
+		}))
+		.pipe(gulp.dest(options.pug.dest));
+
+});
+
+/* ------------------------- *
+ *          BUILD
+ * ------------------------- */
+ gulp.task('default', function(callback){
+ 	runSequence(['copy', 'pug'],
+ 		callback);
+ });
+
+
+/* ------------------------- *
+ *       THE WATCHER
+ * ------------------------- */
+// Follow this syntax
+// gulp.task('watch', ['array', 'of', 'tasks', 'to', 'complete','before', 'watch'], function (){
+
+gulp.task('watch', ['copy', 'pug'] ,function(){
+	gulp.watch(options.pug.src, ['pug']); // pug
+});
+
